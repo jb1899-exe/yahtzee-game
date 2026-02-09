2455f409fa720bb19175a1dc2e4da630e01ab3c7
buchananja <buchanan.j.a@proton.me>
2026-02-09 17:43:19 +0000

    Add primitive reroll functionality.

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Game:
     def update_player_names(self):
         while True:
             new_player_response = input("Would you like to enter a player name (yes/no)?: ")
-            if new_player_response.lower() in ['yes', 'y']:
+            if new_player_response.lower() in['yes', 'y']:
                 while True:
                     name_response = input("Please enter a player name: ")
                     self.player_names.append(name_response)
@@ -60,15 +60,44 @@ class Game:
     def get_player_names(self):
         return self.player_names
     
-    def play_round(self):
-        pass
+    def roll_round(self, dice_result = None, keep = ["a", "b", "c", "d", "e"]):
+        if dice_result is None:
+            dice_result = {}
+        for die_id in keep:
+            die = Die()
+            roll_result = die.get_value()
+            dice_result.update({die_id: roll_result})
+        return dice_result
+            
 
 
 def main():
     
     game = Game()
-    game.update_player_names()
-    game.play_round()
+    # game.update_player_names()
+    for round in range(1, 14):
+
+        print(f"Round {round}!\n")
+
+        # play_round
+        initial_roll = game.roll_round(dice_result = {}) 
+        for die, result in initial_roll.items():
+            print(f"Die {die.upper()} rolled {result}!")
+
+        reroll_attempts = 0
+        while True and reroll_attempts < 2:
+            reroll_input = input("\nWould you like to reroll? (yes/no): ")
+            if reroll_input.lower() in ['yes', 'y']:
+                reroll_attempts += 1
+                game.roll_round(initial_roll)
+                for die, result in initial_roll.items():
+                    print(f"Die {die.upper()} rolled {result}!")
+            elif reroll_input.lower() in ['no', 'n']:
+                break
+            else:
+                print("Please enter a valid response!")
+
+        break # for testing
     
     
 
