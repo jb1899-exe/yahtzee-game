e7937e53f2258799a3cc07676f552096ff810d7f
buchananja <buchanan.j.a@proton.me>
2026-02-11 18:21:28 +0000

    Fix player absent from play_round function.

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from modules.game import Game
 
 def main():
     game = Game()
-    game.update_player_names()
+    game.update_player_names() #TODO: 'No' response does not quit program
     for round in range(1, 14):
-        print(f"\nRound {round}!\n")
+        print(f"\nRound {round}!")
         time.sleep(1)
         for player in game.get_player_names():
-            print(f"Player '{player.title()}', it's your turn!\n")
+            print(f"\nPlayer '{player.title()}', it's your turn!")
             time.sleep(1)
-            game.play_round()
+            game.play_round(player)
 
 
 if __name__ == "__main__":
